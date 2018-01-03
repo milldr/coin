02feaa68fc6ebb8a875d70a971517de8aa7568eb
Daniel Miller <miller.6227@osu.edu>
2018-01-03 11:36:03 -0800

    retitled and added all the sections with floating divs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Coins'});
+  res.render('index', { title: 'Sh*tCoin'});
 });
 
 module.exports = router;
